fd66077aace7802ba679e3b55e5ad77676077669
hanane20sa <greendino06@gmail.com>
2023-04-20 19:00:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+ptintf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
