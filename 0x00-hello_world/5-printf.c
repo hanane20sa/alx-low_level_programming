45237ffc6e7661032011a5195dfe28025fb90e6e
hanane20sa <greendino06@gmail.com>
2023-04-20 19:12:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A programm that prints a line with printf function
- * Return: 0 (success)
+ *main - A programm that prints a line using printf function
+ *Return: 0 (success)
  */
 int main(void)
 { ptintf("with proper grammar, but the outcome is a piece of art,\n");
