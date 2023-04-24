e77039b0e510c613eae6641b2cf663d57cfc5e8d
hanane20sa <greendino06@gmail.com>
2023-04-24 07:53:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'A programm that prints alphabet in lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
