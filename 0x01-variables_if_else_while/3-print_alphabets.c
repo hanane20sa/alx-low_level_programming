1a076cceaf3453151d7e727ca101c4514b32b365
hanane20sa <greendino06@gmail.com>
2023-04-24 09:17:12 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char ch = 'a';
+	if char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	char ch = 'A';
+	if char = 'A';
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
