5a47facd57b8fa067b7800e8a98a47f6c8393196
hanane20sa <greendino06@gmail.com>
2023-04-26 18:27:42 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d,", n);
+	printf("%d, ", n);
 	}
 	}
 	}
@@ -32,13 +32,13 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printf("%d", n);
+	printf("%d" , n);
 	printf("\n");
 	break;
 	}
 	else
 	{
-	printf("%d,", n);
+	printf("%d ,", n);
 	}
 	}
 	}
