b89eb6197c27c18baa0a84df507660d02d1bf1d9
hanane20sa <greendino06@gmail.com>
2023-04-27 17:09:34 -0700

    ./0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,12 @@
  *Return: 1 for uppercase or 0 for anyrhing else
  */
 
-int _isupper(int c);
+int _isupper(int c)
 
 {
 	if (c >= 65 && c <= 90)
 	{
-	return (1);	
+	return (1);
 	}
 
 	return (0);
