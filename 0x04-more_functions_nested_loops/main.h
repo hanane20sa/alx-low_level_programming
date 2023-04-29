47919ce27901ab145b24edc780b62bf8a8dbcbe0
hanane20sa <greendino06@gmail.com>
2023-04-29 06:44:10 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,4 +6,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-_putchar;
+void print_diagonal(int n);
