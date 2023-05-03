da5634f2c5c3786d8eb1e1df213d5519c73a361f
hanane20sa <greendino06@gmail.com>
2023-05-02 18:20:31 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int as a parameter
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
