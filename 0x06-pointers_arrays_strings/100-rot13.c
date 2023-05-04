2841ef5e5ea3f26d20e124d5d301c787f2a602bd
hanane20sa <greendino06@gmail.com>
2023-05-04 13:19:59 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,28 +3,41 @@
 
 /**
  * rot13 - A function that encodes rot13
- * @s: pointer to string params
- * Return: *s
+ * @str: The string to be encoded.
+ * Return: a pointer to the encoded string
  */
 
-char *rot13(char *s)
+char *rot13(char *str)
 
 {
-	int l;
-	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwyzabcdefghijklm";
-
-	for (l = 0; s[l] != '\0'; l++)
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
+			'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'i', 'j', 'k', 'l', 'm', 'n', 'o',
+			'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G',
+			'H', 'I', 'J', 'K', 'L', 'M', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u',
+			'v', 'w', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm'};
+	while (str[indx1])
 	{
-		for (j = 0; j < 52; j++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (s[l] == data1[j])
+			if (str[indx1] == alphabet[indx2])
 			{
-				s[l] = datarot[j];
+				str[indx1] = rot13key[indx2];
 				break;
 			}
 		}
+		indx1++;
 	}
-	return (s);
+	return (str);
 }
