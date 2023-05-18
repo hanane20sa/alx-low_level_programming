3cd52738019d30ce16600dfc9b083c36a66bc0a1
hanane20sa <greendino06@gmail.com>
2023-05-17 17:42:30 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_word - count number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
+ */
+
+int count_word(char *s)
+{
+	int flag, c, w;
