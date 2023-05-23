0a86b4c2bd7544122ee19defdaaa138dd0a2a99e
hanane20sa <greendino06@gmail.com>
2023-05-23 16:10:21 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,5 +14,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 	printf("name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
 }
